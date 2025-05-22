00388ad394d63a97c2081002d648a1847ce7cce8
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-05-22 14:56:45 -0400

    Alex_May-22

---
+++ script.js
@@ -0,0 +1,11 @@
+const hungerBar = document.querySelector('#hungerBar')
+const feedBtn = document.querySelector('#feed')
+const foodAmount = document.querySelector('#availableFood')
+
+let availableFood = 0
+let hungerValue = 5
+
+
+
+
+
