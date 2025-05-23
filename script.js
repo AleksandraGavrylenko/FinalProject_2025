c0288745eb289a7e15fa040517b77e790e2153a7
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-05-23 12:29:46 -0400

    Alex_May-23

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const hungerBar = document.querySelector('#hungerBar')
 const feedBtn = document.querySelector('#feed')
 const foodAmount = document.querySelector('#availableFood')
+const hungerBarList = document.querySelector('#hungerBarList')
 
-let availableFood = 0
-let hungerValue = 5
+let availableFood = 5
+
+function storeHungerValue(){
+    let hungerValue = 5
+    localStorage.setItem('hungerValue', hungerValue)
+    let availableFood = 5
+    localStorage.setItem('availableFood', availableFood)
+}
+
+
+function feed(){
+    if(availableFood>0){
+       hungerValue = localStorage.getItem('hungerValue')
+        availableFood--
+        hungerValue++
+    }
+}
+
+function createHungerBar(){
+    hungerValue = localStorage.getItem('hungerValue')
+    for(i=0;i<hungerValue;i++){
+        const li=document.createElement('li')
+        li.className = 'hungerBarItem'
+        li.textContent='[]'
+        hungerBarList.appendChild(li)
+        console.log('li appended')
+    }
+}
+
+
+
+function innitApp(){
+storeHungerValue()
+createHungerBar()
+}
+
+innitApp()
 
 
 
