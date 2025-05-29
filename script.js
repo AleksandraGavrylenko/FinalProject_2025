e27715729b69a40d66208c6ae002a4e4d6b8c22c
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-05-29 14:56:53 -0400

    Co-authored-by: Gabby Shaka <gabby-shaka@users.noreply.github.com> 5/29/2025

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hungerBar = document.querySelector('#hungerBar')
 const feedBtn = document.querySelector('#feed')
 const foodAmount = document.querySelector('#availableFood')
 const hungerBarList = document.querySelector('#hungerBarList')
+const availableFoodBar = document.querySelector('#availableFood')
 
 //storeHungerValue(5,5)
 
@@ -14,13 +15,13 @@ function storeHungerValue(a,b){
 
 
 function feed(){
-   availableFood = localStorage.getItem('availableFood')
+   let availableFood = localStorage.getItem('availableFood')
     if(availableFood>0){
        hungerValue = localStorage.getItem('hungerValue')
         availableFood--
         hungerValue++
         storeHungerValue(hungerValue,availableFood)
-        createHungerBar()
+        updateStatusBars()
     }
     else{
         alert('no food available right now')
@@ -28,12 +29,15 @@ function feed(){
 }
 
 function addFood(){
-    availableFood=localStorage.getItem('availableFood')
+    let availableFood=localStorage.getItem('availableFood')
+    availableFood++
+    localStorage.setItem('availableFood', availableFood)
+    updateStatusBars()
 }
 
 function createHungerBar(){
     hungerBarList.innerHTML = ''
-    hungerValue = localStorage.getItem('hungerValue')
+    let hungerValue = localStorage.getItem('hungerValue')
     for(i=0;i<hungerValue;i++){
         const li=document.createElement('li')
         li.className = 'hungerBarItem'
@@ -42,16 +46,55 @@ function createHungerBar(){
         console.log('li appended')
     }
 }
-
+function createAvailableFoodBar(){
+    availableFoodBar.innerHTML = ''
+    let availableFood = localStorage.getItem('availableFood')
+    for(i=0;i<availableFood;i++){
+        const li=document.createElement('li')
+        li.className = 'availableFoodItem'
+        Image = document.createElement('img')
+        Image.src = 'images/banana.png'
+        availableFoodBar.appendChild(li)
+        console.log('li appended')
+    }
+}
+function updateStatusBars(){
+    createAvailableFoodBar()
+    createHungerBar()
+    faceChanger()
+}
+setInterval(()=>{
+    let hungerValue = localStorage.getItem('hungerValue')
+    if(hungerValue <= 0){
+        alert('pls feed ur pet!')
+    }
+    else{
+    hungerValue --
+    localStorage.setItem('hungerValue', hungerValue)
+    updateStatusBars()
+    }
+},60000)
 
 
 document.querySelector('#feedBtn').addEventListener('click',feed)
 document.querySelector('#moreFoodBtn').addEventListener('click', addFood )
 window.addEventListener('DOMContentLoaded',()=>{
-    createHungerBar()
+    updateStatusBars()
 })
 
 
 
 
-
+function faceChanger(){
+    let hungerValue = localStorage.getItem('hungerValue')
+    if(hungerValue<3){
+        document.querySelector('#face').src = 'images/thumbnail_angry monkey.png'
+    }
+    else if(hungerValue<6){
+        document.querySelector('#face').src = 'images/thumbnail_happy monkey.png'
+    }
+    else if(hungerValue<9){
+        document.querySelector('#face').src = 'images/thumbnail_fed monkey.png'
+    }
+    
+}
